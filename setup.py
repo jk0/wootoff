7cd9034ef944ce33a9d03821916114de7a06aabf
Josh Kearney <josh@jk0.org>
2011-10-11 13:09:55 -0500

    Quick fixes.

--- setup.py
+++ setup.py
@@ -17,7 +17,7 @@ import setuptools
 
 setuptools.setup(
     name="wootoff",
-    version="0.0.1",
+    version="0.0.2",
     author="Josh Kearney",
     author_email="josh@jk0.org",
     description="Monitor a wootoff.",
